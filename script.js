f597634484f1cdbad54db7cdee7b4d60c8795202
Lab's <Vous@exemple.com>
2019-03-29 17:17:43 +0100

    javaScript hey git status

---
+++ script.js
@@ -0,0 +1,17 @@
+<SCRIPT LANGUAGE="JavaScript">
+
+          // Question
+          var rep = confirm("Voulez-vous aller sur xxxvidsxxx les kinous ?")
+          
+          if(rep == true)
+          {
+          // url d'envoi si Ok est cliqué
+          self.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+          }
+          else
+          {
+          // url d'envoi si Annulé est cliqué
+          self.location.href="https://www.google.com/search?q=zidane&client=ubuntu&hs=rOZ&channel=fs&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjkjrrHuqDhAhXMBWMBHRquAocQ_AUIDygC&biw=1533&bih=748#imgrc=bezPJG0iHzfR3M:";
+          }
+          
+</SCRIPT>
